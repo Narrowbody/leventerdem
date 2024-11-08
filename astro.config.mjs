0e8498717d121f07f842856e5e065873c809ef48
Narrowbody <narrowbody@gmail.com>
2024-11-08 18:54:20 +0100

    Revert "added exisiting site"
    This reverts commit 2b6222e9a875cff8bdd2bb4a37c46fc7da4154f5.

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,19 +3,12 @@ import { defineConfig } from 'astro/config';
 
 import cloudflare from '@astrojs/cloudflare';
 
-import icon from 'astro-icon';
-
-import tailwind from '@astrojs/tailwind';
-
-import react from '@astrojs/react';
-
 // https://astro.build/config
 export default defineConfig({
-	integrations: [icon(), tailwind(), react()],
-	output: 'server',
-	adapter: cloudflare({
-		platformProxy: {
-			enabled: true,
-		},
-	}),
+  output: 'server',
+  adapter: cloudflare({
+    platformProxy: {
+      enabled: true
+    }
+  })
 });
--- src/env.d.ts
+++ src/env.d.ts
@@ -1,7 +1,5 @@
 /// <reference path="../.astro/types.d.ts" />
-///<reference path="../.astro/types.d.ts" />
-///<reference path="astro-icon/components" />
-type Runtime = import('@astrojs/cloudflare').Runtime<Env>;
+type Runtime = import("@astrojs/cloudflare").Runtime<Env>;
 
 declare namespace App {
 	interface Locals extends Runtime {}
